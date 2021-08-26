6a9048469297712b4af489646208985fa549fb1f
Deivid <deivid.cardos@gmail.com>
2021-08-25 22:35:55 -0300

    solve bug in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/global-styles.css";
+import { Home } from "./templates/Home";
 
 ReactDOM.render(
   <React.StrictMode>
