5305e6ab75f55b51e3bdc42eeced7ea912dbff86
Deivid <deivid.cardos@gmail.com>
2021-08-25 22:35:31 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/global-styles.css";
-import { Home } from "./templates/Home";
 
 ReactDOM.render(
   <React.StrictMode>
